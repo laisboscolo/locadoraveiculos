2700f03b2506f72140697256910371a6a51acf1a
Laís Boscolo <costaboscololais2008@gmail.com>
2025-04-14 09:25:19 -0300

    Update perfil_adm.php

--- perfil_adm.php
+++ perfil_adm.php
@@ -104,11 +104,62 @@
                             </label>
                             <input type="number" name="dias_calculo" class="form-control" value="1" required>
                         </div>
+                        <button class="btn btn-success w-100" type="submit" name="calcular">Calcular previsão</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- agora sim sou eu -->
+    <!-- tabela de veiculos cadastrados -->
+    <div class="row mt-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="mb-0">
+                        <!-- mb- margin botton -->
+                        Veículos Cadastrados 🚗
+                    </h4>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <th>Tipo</th>
+                                <th>Modelo</th>
+                                <th>Placa</th>
+                                <th>Status</th>
+                                <th>Ações</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <!-- tr = table row -->
+                                    <td>Carro</td>
+                                    <td>Uno</td>
+                                    <td>21LA1PV</td>
+                                    <td>
+                                        <span class="badge bg-sucess">
+                                            Disponivel ✅
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <div class="action-wrapper">
+                                            
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
+
+
 </body>
 </html>
